a6b1f6ff09d622390180adeff2bc65c9a2c12cfd
Matthew Bly <skyguard14@iCloud.com>
2020-05-27 12:19:34 -0700

    Code For Module 6 Lab 1

---
+++ Module 6 Lab 1.py
@@ -0,0 +1,52 @@
+"""
+CTEC 121
+Matthew Bly
+Module 6 Lab 1
+Code for Module 6 Lab 1
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    '''
+    # definite loop example
+    for i in range(0 , 11, 2):
+        print(i)
+    print()
+
+    # same behavior but coded as indefinite or while loop
+    count = 0
+    while(count < 11):
+        print(count)
+        #count = count + 2
+        count += 2
+
+
+    x = True
+    y = True
+    if(x):
+        if(y):
+            print('both true')
+        else:
+            print('y false')
+    else:
+        print('x false')
+
+    if x and y:
+        print('both true')
+    else:
+        print('other')
+
+    print()
+    '''
+
+    print(True and False)
+    print(True and True)
+    print(True or False)
+    print(False or False)
+    
+main()    
